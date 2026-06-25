4aa755dc29bd1fd3521e7a6c9cb73fc7625bb1c1
Vithurshan Varenthiraraja <64639291+vvithurshan@users.noreply.github.com>
2026-06-24 20:38:12 -0600

    Add: find-duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        
+        l, r = 1, len(nums) - 1
+
+        while l < r:
+
+            mid = l + (r - l)//2
+
+            lessOrEqual = sum(1 for num in nums if num <= mid)
+
+            if lessOrEqual <= mid:
+                l = mid + 1
+
+            else:
+                r = mid 
+
+        return l
