91e92b4a34cd0f1ce94a7dcca28e5314a7bd2bea
Vithurshan Varenthiraraja <64639291+vvithurshan@users.noreply.github.com>
2026-06-26 20:23:13 -0600

    Add: is-palindrome - submission-8

---
+++ Data Structures & Algorithms/is-palindrome/submission-8.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        new = ""
+
+        for c in s:
+            if c.isalnum():
+                new += c.lower()
+
+        return new == new[::-1]
