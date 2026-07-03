e636fa7c6aa3c24c3f4ac5d02f01236a6d6d51da
Vithurshan Varenthiraraja <64639291+vvithurshan@users.noreply.github.com>
2026-07-03 00:30:57 -0600

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -0,0 +1,42 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()    
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+
+            cur = cur.children[c]
+
+        cur.endOfWord = True
+
+    def search(self, word: str) -> bool:
+        
+        def dfs(i, root):
+            cur = root
+            for j in range(i, len(word)):
+                c = word[j]
+
+                if c == ".":
+                    for children in cur.children.values():
+                        if dfs(j + 1, children):
+                            return True
+                    return False
+
+                else:
+                    if c not in cur.children:
+                        return False
+                    cur = cur.children[c]
+            return cur.endOfWord
+
+        return dfs(0, self.root)
+
